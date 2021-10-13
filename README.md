3bd02399be94cd4d7d749716e09403b3cfa4008b
Antonette <ortizantonette@gmai.com>
2021-10-13 16:12:25 -0400

    added function to hide display

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ A library of Garifuna leaders, featuring members of the Garifuna community and a
 - Working with grid
 - Grid template columns
 - Grid: Auto Fill vs Auto Fit
-- image contain
+- Using css property object-fit to fit images in card.
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let heroes = [
     
     {
         name: "Thomas Vincent",
-        bio: "Born on September 17, 1887 in Puerto Cortes, Honduras, Thomas Vincent Ramos not only molded young minds as a school teacher, he also founded the Carib Development and Sick Aid Society (CDS) as well as the Carib International Society (CIS). Both organizations aimed to offer support to the most vulnerable and had a prominent presence in Garifuna communities in Belize, as well as affiliations in Guatemala and Honduras. <br /> After Ramos married, he and his wife moved to Dangriga, Belize in 1920. While there, he fought for improved health facilities and lobbied for native nurses to serve Garinagu in hospitals. <br /> In an effort to preserve and celebrate the Garifuna cultural heritage, Ramos – along with Pantaleon Hernandez and Domingo Ventura – petitioned the British Governor to establish a public and bank holiday in observance of the group’s arrival to Belize on November 19. The day became an official national holiday in 1977."
+        bio: "Born on September 17, 1887 in Puerto Cortes, Honduras, Thomas Vincent Ramos not only molded young minds as a school teacher, he also founded the Carib Development and Sick Aid Society (CDS) as well as the Carib International Society (CIS). Both organizations aimed to offer support to the most vulnerable and had a prominent presence in Garifuna communities in Belize, as well as affiliations in Guatemala and Honduras. <hr>After Ramos married, he and his wife moved to Dangriga, Belize in 1920. While there, he fought for improved health facilities and lobbied for native nurses to serve Garinagu in hospitals. <hr>In an effort to preserve and celebrate the Garifuna cultural heritage, Ramos – along with Pantaleon Hernandez and Domingo Ventura – petitioned the British Governor to establish a public and bank holiday in observance of the group’s arrival to Belize on November 19. The day became an official national holiday in 1977."
     },
     
     
@@ -18,13 +18,13 @@ let heroes = [
     
     {
         name: "Miriam Miranda",
-        bio: "Miriam Miranda is an activist and the current leader of the OFRANEH. The organization works with Garifuna and indigenous communities in Honduras to protect communities’ economic, social, and cultural rights. The organization also promotes health and environmental education, supports the use of the Garifuna language, and promotes youth and women’s leadership development. Miranda has dedicated her life to the cause, and as a result, has been threatened and kidnapped. In 2015, Miranda succeeded in bringing Canadian investor Randy Jorgensen to trial in Trujillo, Honduras for his mega-tourism project built illegally on Garifuna land. <br /> “We live almost on the sea, right on the beach,” she said. “It’s a blessing but recently it’s also become a curse, because of course all those with power want to have a place on the beach. The displacement of communities and the loss of cultures that come with the development of tourism is growing… but the Garifuna women, many of them elders, have incredible strength. They participate in meetings, in actions, tearing down walls that are built on the beach. They’re sustaining the Garifuna youth so that they know who they are, without shame.”"
+        bio: "Miriam Miranda is an activist and the current leader of the OFRANEH. The organization works with Garifuna and indigenous communities in Honduras to protect communities’ economic, social, and cultural rights. The organization also promotes health and environmental education, supports the use of the Garifuna language, and promotes youth and women’s leadership development. Miranda has dedicated her life to the cause, and as a result, has been threatened and kidnapped. In 2015, Miranda succeeded in bringing Canadian investor Randy Jorgensen to trial in Trujillo, Honduras for his mega-tourism project built illegally on Garifuna land. <hr>“We live almost on the sea, right on the beach,” she said. “It’s a blessing but recently it’s also become a curse, because of course all those with power want to have a place on the beach. The displacement of communities and the loss of cultures that come with the development of tourism is growing… but the Garifuna women, many of them elders, have incredible strength. They participate in meetings, in actions, tearing down walls that are built on the beach. They’re sustaining the Garifuna youth so that they know who they are, without shame.”"
     },
     
     
     {
         name: "Jose Ávila",
-        bio: "Hailing from Honduras, Jose Ávila is an important figure to Garifuna community in the Bronx, which has a sizable Garinagu population. He moved to the New York borough in 2006 and has actively worked to empower Garifuna people. <br />Ávila is the founder of the New Horizon Investment Club, an organization that promotes the financial empowerment and economic development of the Garinagu. He organizes cultural gatherings, award ceremonies, and Garifuna Month in the Bronx. He’s the managing Member of Garifuna Afro-Latino Entertainment LLC, where he oversees the global entertainment business, including Garifuna Music Group, Garifuna Music Publishing Group, and Gollywood (Garifuna Cinema), which includes motion picture, television, and digital content production and distribution, and the development of new entertainment products, services, and technologies.<br />He is also Chairman of the Board of the Garifuna Coalition USA, Inc. In 2009, borough President Rubén Díaz Jr. appointed him to the Bronx Community Board 6 to further serve his neighborhood."
+        bio: "Hailing from Honduras, Jose Ávila is an important figure to Garifuna community in the Bronx, which has a sizable Garinagu population. He moved to the New York borough in 2006 and has actively worked to empower Garifuna people. <hr>Ávila is the founder of the New Horizon Investment Club, an organization that promotes the financial empowerment and economic development of the Garinagu. He organizes cultural gatherings, award ceremonies, and Garifuna Month in the Bronx. He’s the managing Member of Garifuna Afro-Latino Entertainment LLC, where he oversees the global entertainment business, including Garifuna Music Group, Garifuna Music Publishing Group, and Gollywood (Garifuna Cinema), which includes motion picture, television, and digital content production and distribution, and the development of new entertainment products, services, and technologies.<hr>He is also Chairman of the Board of the Garifuna Coalition USA, Inc. In 2009, borough President Rubén Díaz Jr. appointed him to the Bronx Community Board 6 to further serve his neighborhood."
     },
 
     {
@@ -58,14 +58,27 @@ for (let i = 0; i < bttn.length; i++) {
       let paragraph = document.createElement("p");
       let bio = document.createTextNode(heroes[i].bio);
       paragraph.appendChild(bio);
+      paragraph.innerHTML = heroes[i].bio
       hero[i].appendChild(paragraph);
+      
     },
-
     
+
     // only listen for button click once
     {once : true}
     );
 }
+
+// function to hide display once user clicks button for a second time
+function hideDisplay() {
+let aboutHero = document.querySelector("p")
+if (aboutHero.display === "none") {
+      aboutHero.style.display = "block";
+      } else {
+        aboutHero.style.display = "none";
+      }
+}
+
 
 submit.addEventListener("click", function (e) {
     e.preventDefault();
